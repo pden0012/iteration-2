ab00c3ce2d187a33adc17008d75a6b9675a1d66b
pengfei <pden0012@student.monash.edu>
2025-09-04 21:14:24 +1000

    feat(auth): add simple front-gate password page and deferred mount

--- Iteration 1/iteration 1/src/main.js
+++ Iteration 1/iteration 1/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const mount = () => createApp(App).mount('#app')
+window.__mountApp__ = mount
+
+if (window.__authOk__) {
+  mount()
+}
